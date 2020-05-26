627a2305e0bafa6c2d9ea6b588c52cd8e01ddd9e
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-05-26 18:39:43 +0100

    put implemented

--- index.js
+++ index.js
@@ -1,34 +1,132 @@
-// dotenv loads parameters (port and database config) from .env
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const connection = require('./db');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+// respond to requests on `/`
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
 // respond to requests on `/api/users`
 app.get('/api/users', (req, res) => {
-  // send an SQL query to get all users
   connection.query('SELECT * FROM user', (err, results) => {
     if (err) {
-      // If an error has occurred, then the client is informed of the error
       res.status(500).json({
         error: err.message,
         sql: err.sql,
       });
     } else {
-      // If everything went well, we send the result of the SQL query as JSON
       res.json(results);
     }
   });
 });
 
-app.listen(process.env.PORT, (err) => {
+// respond to requests on `/api/users/:id`
+app.get('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+  connection.query('SELECT * FROM user WHERE id = ?', [userId], (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+const userValidationMiddlewares = [
+  check('email').isEmail(),
+  check('password').isLength({ min: 8 }),
+  check('name').isLength({ min: 2 }),
+];
+
+// Insert request for new user
+app.post('/api/users', userValidationMiddlewares, (req, res) => {
+  const formData = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  connection.query('INSERT INTO user SET ?', formData, (err, results) => {
+    if (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(409).json({
+          error: 'Email already exists',
+        });
+      }
+      return res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    }
+    // Show new user information
+    return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      const insertedUser = records[0];
+      const { password, ...user } = insertedUser;
+      const host = req.get('host');
+      const location = `http://${host}${req.url}/${user.id}`;
+      return res
+        .status(201)
+        .set('Location', location)
+        .json(user);
+    });
+  });
+});
+
+// Update request for user
+app.put('/api/users/:id', userValidationMiddlewares, (req, res) => {
+  const idUser = req.params.id;
+  const formData = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUser], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    }
+    // Show updated user information
+    return connection.query('SELECT * FROM user WHERE id = ?', idUser, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      const insertedUser = records[0];
+      const { password, ...user } = insertedUser;
+      const host = req.get('host');
+      const location = `http://${host}${req.url}/${user.id}`;
+      return res
+        .status(200)
+        .set('Location', location)
+        .json(user);
+    });
+  });
+});
+
+app.listen(process.env.PORT || 3001, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
   }
-
-  console.log(`Server is listening on ${process.env.PORT}`);
+  console.log(`Listening on ${process.env.PORT}`);
 });
